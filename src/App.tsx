bc01a88e86cb166f29f110be4c7627dc9933b2c2
Matthew Zirbel <matthew.d.zirbel@gmail.com>
2026-06-05 14:25:09 -0700

    fix: pass the form Id into the thermometer component (#2)
    * fix: pass the form Id into the thermometer component

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ const App = ({ domElement }: { domElement?: Element }) => {
   const startDate = domElement?.getAttribute("data-start-date") ?? "";
   const goal = parseInt(domElement?.getAttribute("data-goal") ?? "0", 10);
   const offset = parseInt(domElement?.getAttribute("data-offset") ?? "0", 10);
+  const formId = domElement?.getAttribute("form-id") ?? "";
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Thermometer startDate={startDate} goal={goal} offset={offset} />
+      <Thermometer startDate={startDate} goal={goal} offset={offset} formId={formId} />
     </QueryClientProvider>
   );
 };
